566f2374b0154b47e4c60d0a02502b042de61503
danielpinto-developer <danielpinto.developer@gmail.com>
2025-08-06 11:06:12 -0600

    footer update

--- src/components/footers/FiveColumnWithInputForm.js
+++ src/components/footers/FiveColumnWithInputForm.js
@@ -6,24 +6,14 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 const Container = tw.div`w-full bg-gray-100 text-gray-700 py-10`;
 const Content = tw.div`max-w-screen-xl mx-auto relative z-10`;
 
-const LinkRow = tw.div`flex flex-wrap justify-center text-center gap-2 text-sm font-medium text-gray-600`;
-const FooterLink = tw.a`hover:underline cursor-pointer`;
-
 const CopywrightNotice = tw.p`mt-8 text-center text-sm sm:text-base font-medium text-gray-500`;
 
 export default () => {
   return (
     <Container>
       <Content>
-        <LinkRow>
-          <FooterLink href="#">Política de Privacidad</FooterLink>
-          <FooterLink href="#">Términos y Condiciones</FooterLink>
-          <FooterLink href="#">Nuestros Socios</FooterLink>
-          <FooterLink href="#">No Vendas Mi Información</FooterLink>
-        </LinkRow>
         <CopywrightNotice>
-          &copy; {new Date().getFullYear()} Confianza Total. Todos los derechos
-          reservados.
+          &copy; {new Date().getFullYear()} ACC Wellness. All Rights Reserved.
         </CopywrightNotice>
       </Content>
     </Container>
